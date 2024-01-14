fecca36376ff109793d930b267df2cd906cbe12c
Francisco Possamai <franpossa1@gmail.com>
2024-01-14 12:19:06 -0300

    added middleware

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,12 +1,13 @@
 import { Request, Response, Router } from "express";
 import { deleteItem, getItem, getItems, postItem,  updateItem } from "../controllers/item.controller";
+import logMiddleware from "../middleware/log";
 
 /**
  * http://localhost:3000/items [GET]
  */
 const itemRouter = Router();
 
-itemRouter.get("/", getItems);
+itemRouter.get("/",logMiddleware, getItems);
 itemRouter.get("/:id", getItem);
 itemRouter.post("/", postItem);
 itemRouter.delete("/:id", deleteItem);
